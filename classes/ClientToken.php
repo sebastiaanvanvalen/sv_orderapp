dd9512a828e5673a1e9b60a1caf9102587a50d25
sebastiaanvanvalen <60898247+sebastiaanvanvalen@users.noreply.github.com>
2020-09-01 13:31:19 +0200

    Update ClientToken.php

--- classes/ClientToken.php
+++ classes/ClientToken.php
@@ -7,10 +7,10 @@ class ClientToken{
         $_SESSION['PASS'] = 'alfredmeesens1950!'; 
         $timestamp   = time() + (7 * 24 * 60);
         $passphrase  = $_SESSION['PASS'];
-        $passphrase .= 'leeloominailekataribalaminatchaiekbatdesebat'; 
-        $passphrase .= $timestamp;
-        $secret      = 'ianmalcolm1995';
-        $algo        = 'sha512';
+        $passphrase .= ''; 
+        $passphrase .= ;
+        $secret      = '';
+        $algo        = '';
 
         return bin2hex(hash_hmac($algo, $passphrase, $secret, true)) . '|' .  $timestamp;
     }
@@ -18,10 +18,10 @@ class ClientToken{
     public function check($key){
         $parts = explode('|', substr($key, 7));
         $passphrase  = $_SESSION['PASS'];
-        $passphrase .= 'leeloominailekataribalaminatchaiekbatdesebat';
-        $passphrase .= $parts[1];
-        $secret      = 'ianmalcolm1995';
-        $algo        = 'sha512';
+        $passphrase .= '';
+        $passphrase .= [1];
+        $secret      = '';
+        $algo        = '';
 
         try{
             if (!hash_equals(hex2bin($parts[0]), hash_hmac($algo, $passphrase, $secret, true))) {
