43af927beda96988c22537a98588724c5c832990
sebastiaanvanvalen <60898247+sebastiaanvanvalen@users.noreply.github.com>
2020-09-01 13:26:38 +0200

    Update define.php

--- config/define.php
+++ config/define.php
@@ -1,25 +1,25 @@
 <?php
 // // oude instellingen
 define('ROOT_URL', 'http://localhost/testing/testsmitenvoogt/index.php');
-define('DB_HOST' , 'localhost');
-define('DB_USER' , 'baxxie');
-define('DB_PASS' , 'HaroldOchtendFussbalSpeakerBrexit21GordijnenHelena');
-define('DB_NAME' , 'testlocalsmitenvoogt');
+define('DB_HOST' , '');
+define('DB_USER' , '');
+define(';
+define('');
 
 // // // old keys
-define('SITE_KEY'  , '6LeKCgAVAAAAAJUIFo8b8xntgpH_jzh4512BGPsn');
-define('SECRET_KEY', '6LeKCgAVAAAAAFOXJccxZKs5uAKaVEjXka2JU5mL');
+define(');
+define(');
 
 // token keys
-define('CLIENT_TOKEN_KEY', 'ianmalcolm1995');
-define('ADMIN_TOKEN_KEY' , '87Gloric!Bashn0trix');
+define('');
+define('');
 
 // gmail inlog
-define('EMAILADRESS', 'appsmitenvoogt@gmail.com');
-define('EMAILPASS'  , 'S&Vapp20=Fex!');
+define('');
+define('');
 
 // cryptoKey
-define('CRYPTO_KEY', '50374351151783244649090046724890');
+define(');
 
 // moet nog op de server!!!
 require_once "../vendor/autoload.php";
